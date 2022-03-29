b83f798c079ecc3733638b9f90600d6975c2e128
AbdullahGumi <abdulgumi77@gmail.com>
2022-03-29 21:33:37 +0100

    Implement left pane and right pane

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,73 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Input } from "antd";
 
+//icons
+import {
+  SearchIcon,
+  NotificationIcon,
+  ChevronDownIcon,
+} from "../Icons/SvgIcons";
+
 //styles
 import "./header.css";
 
 //images
 import { logo, profileImage, mazaryn } from "../../assets";
-
-export const SearchIcon = () => {
-  return (
-    <svg
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333Z"
-        stroke="#AAAAAA"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-      <path
-        d="M17.5 17.5L13.875 13.875"
-        stroke="#AAAAAA"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-  );
-};
-
-export const NotificationIcon = () => {
-  return (
-    <svg
-      width="29"
-      height="29"
-      viewBox="0 0 29 29"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M24.3117 20.5829L21.7383 16.9417V9.82806C21.7383 6.19728 19.051 3.18269 15.561 2.66783V1.06099C15.561 0.475045 15.086 0 14.5001 0C13.9141 0 13.4391 0.475045 13.4391 1.06099V2.66777C9.94913 3.18264 7.26181 6.19722 7.26181 9.828V16.9417L4.68846 20.5829C4.19308 21.2837 4.69475 22.2562 5.55484 22.2562H23.4453C24.3036 22.2563 24.8082 21.2852 24.3117 20.5829Z"
-        fill="#AAAAAA"
-      />
-      <path
-        d="M14.4999 23.0138C12.8495 23.0138 11.5068 24.3564 11.5068 26.0068C11.5068 27.6572 12.8496 29 14.4999 29C16.1503 29 17.4929 27.6573 17.4929 26.0069C17.4929 24.3565 16.1502 23.0138 14.4999 23.0138Z"
-        fill="#AAAAAA"
-      />
-    </svg>
-  );
-};
-
-export const ChevronDown = () => {
-  return (
-    <svg
-      width="14"
-      height="7"
-      viewBox="0 0 14 7"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M1 1L7.28571 6L13 1" stroke="#585858" />
-    </svg>
-  );
-};
 
 const Header = () => {
   return (
@@ -102,6 +47,7 @@ const Header = () => {
               <img
                 className="rounded-full mr-3"
                 src={mazaryn}
+                alt="balance"
                 style={{
                   objectFit: "cover",
                 }}
@@ -125,7 +71,7 @@ const Header = () => {
             <span className="mr-3" style={{ color: "#585858" }}>
               Auwal Rg
             </span>
-            <ChevronDown />
+            <ChevronDownIcon />
           </div>
         </div>
       </header>
--- src/components/left pane/LeftPane.js
+++ src/components/left pane/LeftPane.js
@@ -1,61 +1,305 @@
-//import './LeftPane.css';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import 'remixicon/fonts/remixicon.css'
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
 
-function LeftPane(props){
-    const logout = () => {
-        localStorage.removeItem("auth_token")
-        props.history.push("/login")
-      }
+//images
+import { profileImage } from "../../assets";
 
-      let profileImage = process.env.PUBLIC_URL + '/assets/images/profile.svg'
-    
-      return (
-        <div className=' w-1/4 h-screen flex flex-col justify-center items-center sticky top-0'>
-          <div className=" w-full flex justify-end pb-10">
-            <div className=" w-1/2">
-              <img className=" h-20 w-20" src={profileImage} alt="profile image"/>
-              <p className=" text-left font-bold pt-2">Teddy</p>
+//icons
+import {
+  HomeIcon,
+  ChatIcon,
+  UserFriendsIcon,
+  SettingsIcon,
+  WalletIcon,
+  MyProductsIcon,
+  OffersIcon,
+  JobsIcon,
+  MarketPlaceIcon,
+  PagesIcon,
+} from "../Icons/SvgIcons";
 
-            </div>
-          </div>
-          <nav className=' flex justify-end w-full'>
-            <ul className="w-3/5">
-              <li className=''>
-                <Link to='/' className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
-                  <i class="ri-home-3-line pr-3"></i>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to='/wallet' className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
-                  <i class="ri-wallet-3-line pr-3"></i>
-                  Wallet
-                </Link>
-              </li>
-              <li>
-                <Link to='/notifications' className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
-                  <i class="ri-notification-2-line pr-3"></i>
-                  Notifications
-                </Link>
-              </li>
-              <li>
-                <Link className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
-                  <i className="ri-message-3-line pr-3"></i>
-                  Message
-                </Link>
-              </li>
-              {/* 
-              <li onClick={logout} className="cursor-pointer flex flex-row">
-                <ExitToAppIcon />
-                <p>Exit</p>
-              </li> 
-              */}
-            </ul>
-          </nav>
-        </div>
-      );
+//styles
+import "./LeftPane.css";
+
+function LeftPane(props) {
+  const history = useHistory();
+  const logout = () => {
+    localStorage.removeItem("auth_token");
+    props.history.push("/login");
+  };
+
+  return (
+    <div
+      className="leftPane h-screen flex flex-col justify-center items-center sticky top-0"
+      style={{
+        padding: "0% 3%",
+        width: "23%",
+        paddingTop: "25rem",
+        paddingBottom: "5rem",
+      }}
+    >
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "start",
+          background: "white",
+          width: "100%",
+          borderRadius: "20px",
+          boxShadow: "0px 10px 20px rgba(112, 144, 176, 0.12)",
+          padding: "26px 30px",
+        }}
+      >
+        <ul>
+          <Link to="/">
+            <li className="flex mb-4 items-center">
+              <HomeIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/" ? "#4385F5" : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Home
+              </span>
+            </li>
+          </Link>
+          <Link to="/chat">
+            <li className="flex mb-4 items-center">
+              <ChatIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/chat"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Chat
+              </span>
+            </li>
+          </Link>
+          <Link to="/group">
+            <li className="flex mb-4 items-center">
+              <UserFriendsIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/group"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Group
+              </span>
+            </li>
+          </Link>
+          <Link to="/settings">
+            <li className="flex items-center">
+              <SettingsIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/settings"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Settings
+              </span>
+            </li>
+          </Link>
+        </ul>
+      </nav>
+
+      <nav
+        style={{
+          marginTop: "2rem",
+          display: "flex",
+          justifyContent: "start",
+          background: "white",
+          width: "100%",
+          borderRadius: "20px",
+          boxShadow: "0px 10px 20px rgba(112, 144, 176, 0.12)",
+          padding: "26px 30px",
+        }}
+      >
+        <ul>
+          <Link to="/wallet">
+            <li className="flex mb-4 items-center">
+              <WalletIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/wallet"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Wallet
+              </span>
+            </li>
+          </Link>
+          <Link to="/my-products">
+            <li className="flex mb-4 items-center">
+              <MyProductsIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/my-products"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                My Products
+              </span>
+            </li>
+          </Link>
+          <Link to="/profile">
+            <li className="flex items-center ">
+              <img
+                src={profileImage}
+                alt="profile"
+                className="rounded-full"
+                style={{
+                  width: "20px",
+                  height: "20px",
+                  border:
+                    history.location.pathname === "/profile" &&
+                    "2px solid #4385F5",
+                }}
+              />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/profile"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                @auwalrg
+              </span>
+            </li>
+          </Link>
+        </ul>
+      </nav>
+
+      <span
+        className="self-start"
+        style={{ fontSize: "20px", color: "#AAAAAA", marginTop: "2rem" }}
+      >
+        Explore
+      </span>
+
+      <nav
+        style={{
+          marginTop: "2rem",
+          display: "flex",
+          justifyContent: "start",
+          background: "white",
+          width: "100%",
+          borderRadius: "20px",
+          boxShadow: "0px 10px 20px rgba(112, 144, 176, 0.12)",
+          padding: "26px 30px",
+        }}
+      >
+        <ul>
+          <Link to="/offers">
+            <li className="flex mb-4 items-center">
+              <OffersIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/offers"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Offers
+              </span>
+            </li>
+          </Link>
+          <Link to="/jobs">
+            <li className="flex mb-4 items-center">
+              <JobsIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/jobs"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Jobs
+              </span>
+            </li>
+          </Link>
+          <Link to="/marketplace">
+            <li className="flex mb-4 items-center">
+              <MarketPlaceIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/marketplace"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Marketplace
+              </span>
+            </li>
+          </Link>
+          <Link to="/pages">
+            <li className="flex items-center">
+              <PagesIcon />
+              <span
+                style={{
+                  color:
+                    history.location.pathname === "/pages"
+                      ? "#4385F5"
+                      : "#5D5F63",
+                  fontSize: "20px",
+                  fontWeight: 600,
+                  marginLeft: "1rem",
+                }}
+              >
+                Pages
+              </span>
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default LeftPane;
